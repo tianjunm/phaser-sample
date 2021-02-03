513625736f41d37774f34077aa54f13be307eff0
TJ <tianjunma1997@gmail.com>
2021-02-02 23:19:58 -0800

    incorporate scenes

--- js/game.js
+++ js/game.js
@@ -1,50 +1,50 @@
+class Street extends Phaser.Scene {
+  preload() {
+    this.load.image('background', 'assets/landscape.jpg');
+    this.load.image('dude', 'assets/dude.png');
+    this.load.image('room_background', 'assets/room.jpg');
+  }
+
+  create() {
+    this.background = this.add.image(400, 300, 'background');
+    this.player = this.add.sprite(300, 350, 'dude');
+    this.player.displayWidth = 30;
+    this.player.scaleY = this.player.scaleX;
+    this.cursors = this.input.keyboard.createCursorKeys();
+  }
+
+  update() {
+    if (this.cursors.left.isDown) {
+      this.player.x -= 1;
+    } else if (this.cursors.right.isDown) {
+      this.player.x += 1;
+    } else if (this.cursors.up.isDown) {
+      this.player.y -= 1;
+    } else if (this.cursors.down.isDown) {
+      this.player.y += 1;
+    }
+
+    if (this.player.x >= 350 && this.player.x <= 400 &&
+      this.player.y <= 265) {
+      this.background.setTexture('room_background');
+    }
+  }
+}
+
+class Room extends Phaser.Scene {
+  constructor() {
+    super('Room');
+  }
+
+  create() {
+    this.add.image(400, 300, 'room_background');
+  }
+}
 const config = {
   type: Phaser.AUTO,
   width: 800,
-  height: 600,
-  scene: {
-    preload: preload,
-    create: create,
-    update: update
-  }
+  height: 800,
+  scene: [Street, Room],
 };
 
 var game = new Phaser.Game(config);
-
-function preload ()
-{
-  this.load.image('background', 'assets/landscape.jpg');
-  this.load.image('dude', 'assets/dude.png');
-}
-
-function create ()
-{
-  this.add.image(400, 300, 'background');
-  player = this.add.sprite(100, 45, 'dude');
-  player.displayWidth = 30;
-  player.scaleY = player.scaleX;
-  cursors = this.input.keyboard.createCursorKeys();
-}
-
-function update ()
-{
-  if (cursors.left.isDown)
-  {
-    console.log("left");
-    player.x -= 1;
-  }
-  else if (cursors.right.isDown)
-  {
-    player.x += 1;
-  }
-  else if (cursors.up.isDown)
-  {
-    player.y -= 1;
-  }
-  else if (cursors.down.isDown)
-  {
-    player.y += 1;
-  }
-}
-
-
